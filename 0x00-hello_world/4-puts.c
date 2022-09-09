4d8ad9c6ba50d1beea807132df12ab13bd435112
ibevictory <ibevictory50@gmail.com>
2022-09-09 00:28:07 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
+#include <stio.h>
 
 /**
- * main - print the string in the put function
+ * main - printing
  *
- * Description:using the main function
- * this program prints out "Programming is like building a multilingual puzzle
- * return: 0
+ * Return: Always 0 (Successful)
  */
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\Programming is like building a multilingual puzzle");
+	return 0;
 }
