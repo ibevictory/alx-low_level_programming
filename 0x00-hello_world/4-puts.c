e9449736f81f57d1bc5ca4eb1c4516631a1ad1db
ibevictory <ibevictory50@gmail.com>
2022-09-08 20:34:58 -0700

    putting

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ * 
+ * Description: using the main function
+ * this program would print out 'programming is like a multilingual puzzle
+ * return: 0
+ */
+int main(void){
+	puts("\"programming is like buoilding a mutilingual puzzle");
+	return (0);
+}
