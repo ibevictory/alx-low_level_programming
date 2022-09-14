1105d87abdc18c352ee9e55d617ffe2e80cb417c
ibevictory <ibevictory50@gmail.com>
2022-09-14 15:32:18 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * javck_bauer - function taht prints every minute of tyhe day of jack bauer, starti
+ * jack_bauer - function that prints every minute of the day of Jack Bauer, starti
  * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  * and resets mins
  * Return: 0
