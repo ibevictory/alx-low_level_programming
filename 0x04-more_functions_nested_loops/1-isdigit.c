5061467f7173bb0e96dd42883426644153d55c47
ibevictory <ibevictory50@gmail.com>
2022-09-15 15:24:42 -0700

    commiting

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _sdigit - checks wether a character is a digit or not
+ * @c: tested character
+ * Return: 1 if it is, 0 otherwise
+ */
+int _is digit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	return (0);
+}
