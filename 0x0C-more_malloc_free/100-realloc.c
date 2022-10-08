449a8820ce6527e64f4088d80546d80973c70932
ibevictory <ibevictory50@gmail.com>
2022-10-07 17:22:19 -0700

    commiting

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <sdtlib.h>
+#include <stdlib.h>
 /**
  * _realloc - reallocates a memory block.
  * @ptr: pointer to the memory previously allocated
@@ -16,15 +16,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 	}
+
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
+
 	free(ptr);
 	ptr = malloc(new_size);
+
 	return (ptr)
 }
