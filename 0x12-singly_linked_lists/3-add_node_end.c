c7925ec45eee1c529bd3a3e7a1f3de6153b04eb9
ibevictory <ibevictory50@gmail.com>
2022-10-20 21:28:51 -0700

    commiting update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -36,4 +36,19 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = strdup(str);
 	if (new->str == NULL)
 	{
-
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
+}
